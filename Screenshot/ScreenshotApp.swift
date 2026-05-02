a5e4b7dbbf6f80de5974ffa8b45039e2ee4685e3
CooperMiller0089 <tskmcious@gmail.com>
2026-05-02 20:53:20 +0800

    Initial Commit

---
+++ Screenshot/ScreenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotApp.swift
+//  Screenshot
+//
+//  Created by Sam on 2026/5/2.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
